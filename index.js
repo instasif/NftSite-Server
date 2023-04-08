0df938dc62fb5db3245f45fa39055fabc78ba958
Asif Hossain <asifh9277@gmail.com>
2023-04-08 06:27:46 +0600

    fixed a code yesterday

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -15,7 +15,6 @@ app.get("/", async (req, res) => {
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2lbo3hl.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,11 +25,40 @@ async function run() {
   try {
     const nftCollections = client.db("nftsite").collection("nfts");
 
-    app.get('/nfts', async(req, res) =>{
+    // to display all card
+    app.get("/all-nfts", async (req, res) => {
       const query = {};
       const nfts = await nftCollections.find(query).toArray();
       res.send(nfts);
-    })
+    });
+
+    // to display only 4 card in the Home page
+    app.get("/nfts", async (req, res) => {
+      const query = {};
+      const nfts = await nftCollections.find(query).limit(4).toArray();
+      res.send(nfts);
+    });
+
+    // to visit nfts details page according to nfts id
+    app.get("/nfts/:id", async (req, res) => {
+      try {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: id };
+      const nfts = await nftCollections.find(query).toArray();
+      res.send(nfts);
+      } catch (error) {
+        console.error(error);
+        res,send(error.message)
+      }
+    });
+
+    // to upload new nft's data
+    app.post("/nfts", async (req, res) => {
+      const nfts = req.body;
+      const result = await nftCollections.insertOne(nfts);
+      res.send(result);
+    });
   } finally {
   }
 }
