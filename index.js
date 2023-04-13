da81a50bf42d0ada286cf00a1b095c704ce2de4d
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-04-14 04:20:17 +0600

    file changed by kamrul

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -26,52 +26,53 @@ async function run() {
     const nftCollections = client.db("nftsite").collection("nfts");
     const userCollection = client.db("nftsite").collection("user");
 
-
-    // to display all card        
-    app.get('/all-nfts', async (req, res) => {
+    // to display all card
+    app.get("/all-nfts", async (req, res) => {
       const query = {};
       const nfts = await nftCollections.find(query).toArray();
       res.send(nfts);
-    })
+    });
 
     // to display only 4 card in the Home page
-    app.get('/nfts', async (req, res) => {
+    app.get("/nfts", async (req, res) => {
       const query = {};
       const nfts = await nftCollections.find(query).limit(4).toArray();
       res.send(nfts);
-    })
+    });
 
-    // to visit nfts details page according to nfts id
-    app.get('/nfts/:id', async (req, res) => {
+    // to visit nfts details page according to nfts id    (re-check pendding)
+    app.get("/nfts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { nfts_id: id };
-      const nfts = await nftCollections.find(query).toArray();
-      res.send(nfts)
-    })
+      const query = { _id: ObjectId(id) };
+      const nfts = await nftCollections.findOne(query).toArray();
+      res.send(nfts);
+    });
 
     // to upload new nft's data
-    app.post('/nfts', async (req, res) => {
+    app.post("/nfts", async (req, res) => {
       const nfts = req.body;
-      const result = await nftCollections.insertOne(product);
+      const result = await nftCollections.insertOne(nfts);
       res.send(result);
-    })
+    });
 
-    app.post('/user', async (req, res) => {
+    // to post user information
+
+    app.post("/user", async (req, res) => {
       const user = req.body;
-      const findUser = await userCollection.findOne({ email: user.email })
+      const findUser = await userCollection.findOne({ email: user.email });
       if (findUser === null) {
         const result = await userCollection.insertOne(user);
         return res.send(result);
       }
       res.send({ status: false, message: "user already added by database" });
-    })
+    });
 
+    // to get user information
 
-    app.get('/user', async (req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await userCollection.find({}).toArray();
       res.send(result);
-    })
-
+    });
   } finally {
   }
 }
