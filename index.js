c21542abdb15651aa5617c1e19cf23b95de4db48
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-04-14 01:27:30 +0600

    post user successfull by kamrul

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  res.send("Nft server is running.....");
+  res.send({ status: true, message: "Nft server is running....." });
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2lbo3hl.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -24,38 +23,55 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-     const nftCollections = client.db("nftsite").collection("nfts");
+    const nftCollections = client.db("nftsite").collection("nfts");
+    const userCollection = client.db("nftsite").collection("user");
 
 
     // to display all card        
-    app.get('/all-nfts', async(req, res) =>{
+    app.get('/all-nfts', async (req, res) => {
       const query = {};
       const nfts = await nftCollections.find(query).toArray();
       res.send(nfts);
     })
 
     // to display only 4 card in the Home page
-    app.get('/nfts', async(req, res) =>{
+    app.get('/nfts', async (req, res) => {
       const query = {};
       const nfts = await nftCollections.find(query).limit(4).toArray();
       res.send(nfts);
     })
 
     // to visit nfts details page according to nfts id
-    app.get('/nfts/:id', async(req, res)=>{
+    app.get('/nfts/:id', async (req, res) => {
       const id = req.params.id;
-      const query={nfts_id:id};
+      const query = { nfts_id: id };
       const nfts = await nftCollections.find(query).toArray();
       res.send(nfts)
-  })
-    
+    })
+
     // to upload new nft's data
-    app.post('/nfts',async(req,res)=>{
-    const nfts = req.body;
-    const result = await nftCollections.insertOne(product);
-    res.send(result);
-})
-    
+    app.post('/nfts', async (req, res) => {
+      const nfts = req.body;
+      const result = await nftCollections.insertOne(product);
+      res.send(result);
+    })
+
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      const findUser = await userCollection.findOne({ email: user.email })
+      if (findUser === null) {
+        const result = await userCollection.insertOne(user);
+        return res.send(result);
+      }
+      res.send({ status: false, message: "user already added by database" });
+    })
+
+
+    app.get('/user', async (req, res) => {
+      const result = await userCollection.find({}).toArray();
+      res.send(result);
+    })
+
   } finally {
   }
 }
